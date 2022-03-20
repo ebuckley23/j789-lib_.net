7b6edcffb4bf0e8f27231bbd858b345fdffc4dc4
Emmanuel K. Buckley <ebuckley2389@gmail.com>
2022-03-20 12:28:53 -0500

    added Api extensions for Swagger and unit tests

---
+++ J789.Library.Api.UnitTests/Swagger_Tests.cs
@@ -0,0 +1,98 @@
+using J789.Library.Api.Swagger;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.OpenApi.Models;
+using Moq;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace J789.Library.Api.UnitTests
+{
+    public class Swagger_Tests
+    {
+        [Fact]
+        public void Can_Configure_Authorized_Security_Requirements_Operation_Filter_Correctly()
+        {
+            var filter = new SecurityRequirementsOperationFilter();
+
+            var openApiOp = new OpenApiOperation();
+            var apidesc = new ApiDescription();
+            var schemaRepo = new SchemaRepository();
+            var mockSchemeGen = new Mock<ISchemaGenerator>();
+            MethodInfo mi = typeof(TestApiController).GetMethod(nameof(TestApiController.GetAsync));
+
+            var opFilterCtx = new OperationFilterContext(apidesc, mockSchemeGen.Object, schemaRepo, mi);
+
+            filter.Apply(openApiOp, opFilterCtx);
+
+            Assert.True(openApiOp.Responses.ContainsKey("401"));
+            Assert.True(openApiOp.Responses.ContainsKey("403"));
+            Assert.NotEmpty(openApiOp.Security);
+        }
+
+        [Fact]
+        public void Can_Configure_AllowAnonymous_Security_Requirements_Operation_Filter_Correctly()
+        {
+            var filter = new SecurityRequirementsOperationFilter();
+
+            var openApiOp = new OpenApiOperation();
+            var apidesc = new ApiDescription();
+            var schemaRepo = new SchemaRepository();
+            var mockSchemeGen = new Mock<ISchemaGenerator>();
+            MethodInfo mi = typeof(TestApiController).GetMethod(nameof(TestApiController.GetAnonymousAsync));
+
+            var opFilterCtx = new OperationFilterContext(apidesc, mockSchemeGen.Object, schemaRepo, mi);
+
+            filter.Apply(openApiOp, opFilterCtx);
+
+            Assert.False(openApiOp.Responses.ContainsKey("401"));
+            Assert.False(openApiOp.Responses.ContainsKey("403"));
+            Assert.Empty(openApiOp.Security);
+        }
+
+        [InlineData("https://example.auth0.com", "https://unitests")]
+        [InlineData("https://example.auth0.com/", "https://unitests")]
+        [Theory]
+        public void Can_Configure_Implicit_Authorization_Flow_Correctly(string authority, string audience)
+        {
+            var swaggerGenOpts = new SwaggerGenOptions();
+
+            Assert.Empty(swaggerGenOpts.SwaggerGeneratorOptions.SecuritySchemes);
+
+            swaggerGenOpts.AddImplicitFlowAuthorization(authority, audience);
+
+            Assert.NotEmpty(swaggerGenOpts.SwaggerGeneratorOptions.SecuritySchemes);
+            Assert.True(swaggerGenOpts.SwaggerGeneratorOptions.SecuritySchemes.ContainsKey("Bearer"));
+
+            var scheme = swaggerGenOpts.SwaggerGeneratorOptions.SecuritySchemes["Bearer"];
+
+            Assert.Equal("Authorization", scheme.Name);
+            Assert.Equal(Microsoft.OpenApi.Models.ParameterLocation.Header, scheme.In);
+            Assert.Equal(Microsoft.OpenApi.Models.SecuritySchemeType.OAuth2, scheme.Type);
+            Assert.NotNull(scheme.Flows);
+            Assert.NotNull(scheme.Flows.Implicit);
+            Assert.NotEmpty(scheme.Flows.Implicit.Scopes);
+            Assert.NotNull(scheme.Flows.Implicit.AuthorizationUrl);
+            Assert.Contains(authority, scheme.Flows.Implicit.AuthorizationUrl.AbsoluteUri);
+            Assert.Contains(audience, scheme.Flows.Implicit.AuthorizationUrl.AbsoluteUri);
+        }
+    }
+
+    internal class TestApiController : ControllerBase
+    {
+        public Task GetAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        [AllowAnonymous]
+        public Task GetAnonymousAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ J789.Library.Api/Swagger/SwaggerExtensions.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace J789.Library.Api.Swagger
+{
+    public static class SwaggerExtensions
+    {
+        /// <summary>
+        /// This will enable the SwaggerUI to be treated as a UI application able to retrieve an authorization token via the implicit flow.
+        /// When attempting to authorize, swagger will be redirected to the configured login page from the authentication authority being used, then
+        /// rerouted back to the swagger UI.
+        /// Important: SwaggerUI url should be configured as a valid callback url with the authentication authority
+        /// </summary>
+        /// <param name="options"></param>
+        /// <param name="authority"></param>
+        /// <param name="audience"></param>
+        public static void AddImplicitFlowAuthorization(this SwaggerGenOptions options, string authority, string audience)
+        {
+            options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                Type = Microsoft.OpenApi.Models.SecuritySchemeType.OAuth2,
+                Flows = new Microsoft.OpenApi.Models.OpenApiOAuthFlows
+                {
+                    Implicit = new Microsoft.OpenApi.Models.OpenApiOAuthFlow
+                    {
+                        Scopes = new Dictionary<string, string> { { "openid", "Open Id" } },
+                        AuthorizationUrl = new Uri(BuildAuthorizationUrl(authority, audience))
+                    }
+                }
+            });
+        }
+
+        private static string BuildAuthorizationUrl(string authority, string audience)
+        {
+            var auth = authority;
+            if (authority.Length - 1 == authority.LastIndexOf("/"))
+            {
+                auth = authority.Substring(0, authority.LastIndexOf("/"));
+            }
+            return $"{auth}/authorize?audience={audience}";
+        }
+    }
+}
